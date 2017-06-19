e5d5136c7de176395879958e5f2311a4f745e890
justin.g.kerr@gmail.com <justin.g.kerr@gmail.com>
2017-06-19 17:48:50 -0400

    initial commit of all server files

---
+++ annotation_writer.py
@@ -0,0 +1,71 @@
+import xml.etree.ElementTree as ET
+import os
+class BoundingBox():
+    xmin,xmax,ymin,ymax=0,0,0,0
+    name=''
+    def __init__(self,name,xmin,ymin,xmax,ymax):
+        self.name=name
+        self.xmin=xmin
+        self.ymin=ymin
+        self.ymax=ymax
+        self.xmax=xmax
+
+
+def save_annotation(imgname,bboxes,savedir,imgwidth,imgheight):
+    '''
+    imgname=name of image file to save eg "0001.jpg"
+    bboxes=list of bounding boxes in the image
+    savedir=root dir to save annotation in
+    '''   
+    name=imgname.split('.')[0]
+    extension='.'+imgname.split('.')[1]
+    annotation=ET.Element('annotation')
+    filename=ET.SubElement(annotation,'filename')
+    filename.text=name+'.jpg'
+    if savedir[-1]!='/':
+        savedir+='/'    
+    size=ET.SubElement(annotation,'size')
+    width=ET.SubElement(size,'width')
+    width.text=str(imgwidth)
+    height=ET.SubElement(size,'height')
+    height.text=str(imgheight)
+    depth=ET.SubElement(size,'depth')
+    depth.text='3'
+    
+    
+    for bbox in bboxes:
+        obj=ET.SubElement(annotation,'object')
+        n=ET.SubElement(obj,'name')
+        n.text=bbox.name
+        bndbox=ET.SubElement(obj,'bndbox')
+        xmin=ET.SubElement(bndbox,'xmin')
+        xmin.text=str(bbox.xmin)
+        ymin=ET.SubElement(bndbox,'ymin')
+        ymin.text=str(bbox.ymin)
+        xmax=ET.SubElement(bndbox,'xmax')
+        xmax.text=str(bbox.xmax)
+        ymax=ET.SubElement(bndbox,'ymax')
+        ymax.text=str(bbox.ymax)     
+        
+        
+    etree=ET.ElementTree(annotation)
+    etree.write(savedir+name+'.xml')
+    return
+
+def load_annotation(imgname):
+    name=imgname.split('.')[0]
+    root=ET.parse('/users/justin/Documents/github/AnnotationApplication/annotationapp/annotationapp/static/annotations/'+name+'.xml')
+    bounding_boxes=[]
+    for obj in root.findall('object'):
+        name=obj.find('name').text
+        bndbox=obj.find('bndbox')
+        xmin=int(float(bndbox.find('xmin').text))
+        ymin=int(float(bndbox.find('ymin').text))
+        xmax=int(float(bndbox.find('xmax').text))
+        ymax=int(float(bndbox.find('ymax').text))
+        bounding_boxes.append(BoundingBox(name,xmin,ymin,xmax,ymax))
+    print(bounding_boxes)
+    return bounding_boxes
+
+if __name__=='__main__':
+    load_annotation('20170618_113023.jpg')
---
+++ annotationapp.py
@@ -0,0 +1,53 @@
+from flask import Flask,render_template,request
+import annotation_writer
+import os
+import random
+app=Flask(__name__)
+
+
+@app.route('/')
+def hi():
+    return render_template('home.html')
+
+#@app.route('/review')
+#def review():
+    #return render_template('review.html')
+
+#@app.route('/getannotation',methods=['POST'])
+#def get_annotation():
+    #print(request.form)
+    #data=request.form
+    #index=data['index']
+    #return "lol"
+
+@app.route('/saveresults',methods=['POST'])
+def save():
+    data=request.form
+    numboxes=int(data['length'])
+    imgname=data['imgname']
+    bboxes=[]
+    for n in range(numboxes):
+        xmin=data['boxes['+str(n)+'][xmin]']
+        xmax=data['boxes['+str(n)+'][xmax]']
+        ymin=data['boxes['+str(n)+'][ymin]']
+        ymax=data['boxes['+str(n)+'][ymax]']
+        name=data['boxes['+str(n)+'][name]']
+        bboxes.append(annotation_writer.BoundingBox(name,xmin,ymin,xmax,ymax))
+    annotation_writer.save_annotation(imgname,bboxes,'/users/justin/Documents/github/AnnotationApplication/annotationapp/annotationapp/static/annotations',data['width'],data['height'])
+    return 'complete'
+
+@app.route('/getimage',methods=['GET'])
+def load_image():
+    annotations=[]
+    for root,dirs,files in os.walk('/users/justin/Documents/github/AnnotationApplication/annotationapp/annotationapp/static/annotations'):
+        annotations=files
+    annotations.pop(0)
+    
+    for root,dirs,images in os.walk('/users/justin/Documents/github/AnnotationApplication/annotationapp/annotationapp/static/images'):
+        images.pop(0)
+        random.shuffle(images)
+        for image in images:
+            name=image.split('.')[0]
+            if annotations.count(name+'.xml')==0:
+                return image
+    return ''   
